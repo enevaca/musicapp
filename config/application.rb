cc7f8ed055cfb4ad29e5de993d1e51a00057b7dc
Noel Vaca <noel.vm.2501@gmail.com>
2018-02-22 15:19:44 -0400

    Integración con APIs de terceros (Spotify) y Búsqueda de canciones

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require'rspotify'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,5 +15,7 @@ module Musicapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+	
+	RSpotify::authenticate(ENV['spotify_client_id'] , ENV['spotify_client_secret'])
   end
 end
